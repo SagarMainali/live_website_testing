1a1c37211de22c8c718dd228342515008eb5242a
Sagar Mainali <77186948+SagarMainali@users.noreply.github.com>
2022-12-06 19:12:03 +0545

    Add files via upload

---
+++ Codes/script.js
@@ -0,0 +1,11 @@
+
+//Jquery for table search
+$(document).ready(function () {
+     $("#myInput").on("keyup", function () {
+          var value = $(this).val().toLowerCase();
+          $("#myTable tr").filter(function () {
+               $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+          });
+     });
+});
+
